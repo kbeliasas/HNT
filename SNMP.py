ed21adfbb21bb28b5648cbe845296a9dc2a02e20
kbeliasas <karolis.beliasas@gmail.com>
2018-01-03 10:37:35 +0200

    Update

--- SNMP.py
+++ SNMP.py
@@ -3,11 +3,12 @@
 import netsnmp
 
 ip = '192.168.226.152'
+print ip
 
 def sys_name(ip):
     oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.1.5'))
     res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip, Community='public')
-    return res
+    return res[0]
 
 def get_nei_name(ip):
     oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.4.1.9.9.23.1.2.1.1.6'))
@@ -30,6 +31,6 @@ ip_add = get_ip_add(ip)
 
 
 for x in range(0, len(nei_name)):
-    device_list[nei_name(x)] = ip_add(x)
+    device_list[nei_name[x] = ip_add[x]
 
 print device_list 
