2db8f2b03162b52679dc50a54d850c212925c179
kbeliasas <karolis.beliasas@gmail.com>
2018-01-03 19:05:40 +0200

    Update

--- SNMP.py
+++ SNMP.py
@@ -4,15 +4,6 @@ import netsnmp
 
 ip = '192.168.226.152'
 
-def sys_name(ip):
-    oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.2.1.1.5'))
-    res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip, Community='public')
-    return res[0]
-
-def get_nei_name(ip):
-    oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.4.1.9.9.23.1.2.1.1.6'))
-    res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip, Community='public')
-    return res
 
 def get_ip_add(ip):
     oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.4.1.9.9.23.1.2.1.1.4'))
@@ -20,18 +11,33 @@ def get_ip_add(ip):
     return res
 
 
-device_list = { sys_name(ip) : ip }
-
-nei_name = get_nei_name(ip)
 ip_add = get_ip_add(ip)
 
+device_list = []
 
-for x in range(0, len(nei_name)):
-    device_list[nei_name[x]] = ip_add[x]
+for x in range(0, len(ip_add)):
+    device_list.append(ip_add[x])
+
+all_list = [device_list]
 
 tested = []
 tested.append(ip)
 
 print tested
+
+
+x = 0
+while (x < 100):
+    for a in range(0, len(all_list[x])):
+        for b in range(0, len(tested)):
+            if (all_list[x][a] == tested[b]):
+                break
+        ip = all_list[x][a]
+        ip_add = get_ip_add(ip)
+
+        
+
+
+
 
 print device_list 
