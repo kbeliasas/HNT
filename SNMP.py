bb9e305243bcec17530ce5826acfa616cbae270d
kbeliasas <karolis.beliasas@gmail.com>
2018-01-03 09:17:52 +0200

    Update

--- SNMP.py
+++ SNMP.py
@@ -9,9 +9,9 @@ def get_nei_name(ip_add):
     res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip_add, Community='public')
     return res
 
-def get_ip_add():
+def get_ip_add(ip_add):
     oid = netsnmp.VarList(netsnmp.Varbind('. 1.3.6.1.4.1.9.9.23.1.2.1.1.4'))
     res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip_add, Community='public')
     return res
 
-print get_nei_name()
+print get_nei_name(ip_add)
