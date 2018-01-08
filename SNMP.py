b44bd2898c73e51a8580981e84e3a0136185ff6e
kbeliasas <karolis.beliasas@gmail.com>
2018-01-08 16:16:41 +0200

    Update

--- SNMP.py
+++ SNMP.py
@@ -2,10 +2,10 @@
 
 import netsnmp
 
-ip = '192.168.226.152' # Pirmas įrenginys
+ip = '192.168.226.152' # Pirmas irenginys
 
 
-def get_ip_add(ip): # Pasiima kaimynus iš įrenginio.
+def get_ip_add(ip): # Pasiima kaimynus is irenginio.
     oid = netsnmp.VarList(netsnmp.Varbind('.1.3.6.1.4.1.9.9.23.1.2.1.1.4'))
     res = netsnmp.snmpwalk(oid, Version = 2, DestHost=ip, Community='public')
     return res
@@ -36,22 +36,22 @@ while (x > -1):
             print "y = " + str(y)
             print "z = " + str(z)
             if (all_list[x][y] == tested[z]): # Jei jau testuotas, tai ...
-                if (len(all_list[x]) > y + 1): # Jei dar liko kaimynų, tai patikrink jį.
+                if (len(all_list[x]) > y + 1): # Jei dar liko kaimynu, tai patikrink ji.
                     y = y + 1
-                elif (len(all_list[x]) <= y + 1): # Jei nebeliko kaimynų, tai ...
-                    if (len(all_list) > x + 1): # Jei yra kitų įrenginių, tai tikrink juos.
+                elif (len(all_list[x]) <= y + 1): # Jei nebeliko kaimynu, tai ...
+                    if (len(all_list) > x + 1): # Jei yra kitu irenginiu, tai tikrink juos.
                         x = x + 1
                         y = 0
                         z = 0
-                    elif (len(all_list) <= x + 1): # Jei nebeliko įrenginių, tai užbaik.
+                    elif (len(all_list) <= x + 1): # Jei nebeliko įrenginiu, tai uzbaik.
                         x = -2
                         y = -2
                         z = -2
                         break
             elif (all_list[x][y] != tested[z]): # Jei dar netestuotas, tai ...
-                if (len(tested) > z + 1): # Jei dar liko pratestuotų sąraše įrenginių, tai peržiūrėk juos.
+                if (len(tested) > z + 1): # Jei dar liko pratestuotu ssrase irenginiu, tai perziurek juos.
                     z = z + 1
-                elif (len(tested) <= z + 1): # Jei nebeliko pratęstuotų sąraše įrenginių, tai ieškok naujų kaimynų.
+                elif (len(tested) <= z + 1): # Jei nebeliko pratestuotu sarase irenginiu, tai ieskok nauju kaimynu.
                     ip = all_list[x][y]
                     ip_add = get_ip_add(ip)
                     device_list = []
@@ -59,14 +59,14 @@ while (x > -1):
                         device_list.append(ip_add[a])
                     all_list.append(device_list)
                     tested.append(ip)
-                    if (len(all_list[x]) > y + 1): # Jei dar liko kaimynų, tai patikrink jį.
+                    if (len(all_list[x]) > y + 1): # Jei dar liko kaimynu, tai patikrink ji.
                         y = y + 1
-                    elif (len(all_list[x]) <= y + 1): # Jei nebeliko kaimynų, tai ...
-                        if (len(all_list) > x + 1): # Jei yra kitų įrenginių, tai tikrink juos.
+                    elif (len(all_list[x]) <= y + 1): # Jei nebeliko kaimynu, tai ...
+                        if (len(all_list) > x + 1): # Jei yra kitu irenginiu, tai tikrink juos.
                             x = x + 1
                             y = 0
                             z = 0
-                        elif (len(all_list) <= x + 1): # Jei nebeliko įrenginių, tai užbaik.
+                        elif (len(all_list) <= x + 1): # Jei nebeliko irenginių, tai uzbaik.
                             x = -2
                             y = -2
                             z = -2
