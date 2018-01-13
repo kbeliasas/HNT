9402252349f38c78a833714c75443ce3cc33c053
kbeliasas <karolis.beliasas@gmail.com>
2018-01-13 18:13:21 +0200

    UP

--- SNMP_test.py
+++ SNMP_test.py
@@ -5,19 +5,18 @@ from pysnmp.hlapi import *
 ip = '192.168.50.100'
 com = 'public'
 
-errorIndication, errorStatus, errorIndex, varBinds = next(
-    getCmd(SnmpEngine(),
-           CommunityData(com),
-           UdpTransportTarget((ip, 161)),
-           ContextData(),
-           ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0')))
-)
+for (errorIndication,
+     errorStatus,
+     errorIndex,
+     varBinds) in nextCmd(SnmpEngine(),CommunityData(com), UdpTransportTarget((ip, 161)), ContextData(), ObjectType(ObjectIdentity('1.0.8802.1.1.2.1.4'))):
 
-if errorIndication:
-    print(errorIndication)
-elif errorStatus:
-    print('%s at %s' % (errorStatus.prettyPrint(),
-                        errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
-else:
-    for varBind in varBinds:
-        print(' = '.join([x.prettyPrint() for x in varBind]))
+    if errorIndication:
+        print(errorIndication)
+        break
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+        break
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
