654efd9ae482320e3684112608f64f917837a977
kbeliasas <karolis.beliasas@gmail.com>
2018-01-13 13:08:03 +0200

    UP

--- SNMP_test.py
+++ SNMP_test.py
@@ -10,8 +10,8 @@ com = 'public'
 
 def get_ip_add(ip):
     session = easysnmp.Session(hostname=ip, version=2, community=com)
-    res = session.get('.1.3.6.1.2.1.1.5.0')
-    return res
+    res = session.get('.1.0.8802.1.1.2.1.3.8.1.2')
+    return res.value
 
 ip_add = get_ip_add(ip)
 print ip_add
