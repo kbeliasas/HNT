2abf37df8fa87db07f4e82e0e8196b3ede65d962
kbeliasas <karolis.beliasas@gmail.com>
2018-01-14 20:44:32 +0200

    Update

--- SNMP.py
+++ SNMP.py
@@ -8,27 +8,31 @@ com = 'public'
 
 def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
     session = easysnmp.Session(hostname=ip, version=2, community=com)
-    res = session.walk('.1.0.8802.1.1.2.1.4.2.1.3')
-    ans = []
-    temp1 = []
-    for item in res:
-        temp1.append(item.oid)
-    c = 0
-    for item in temp1:
-        ans.append('')
-        a = 0
-        b = 1
-        while a < 4:
-            if (item[len(item)-b] == '.'):
-                ans[c] = item[len(item)-b] + ans[c]
-                a = a + 1
-                b = b + 1
-            else:
-                ans[c] = item[len(item)-b] + ans[c]
-                b = b + 1
-        ans[c] = ans[c].lstrip('.')
-        c = c + 1
-    return ans
+    try:
+        res = session.walk('.1.0.8802.1.1.2.1.4.2.1.3')
+        ans = []
+        temp1 = []
+        for item in res:
+            temp1.append(item.oid)
+        c = 0
+        for item in temp1:
+            ans.append('')
+            a = 0
+            b = 1
+            while a < 4:
+                if (item[len(item)-b] == '.'):
+                    ans[c] = item[len(item)-b] + ans[c]
+                    a = a + 1
+                    b = b + 1
+                else:
+                    ans[c] = item[len(item)-b] + ans[c]
+                    b = b + 1
+            ans[c] = ans[c].lstrip('.')
+            c = c + 1
+        return ans
+    except Exception as e:
+        print 'Something wrong on ip = ' + ip_add
+        print(e)
 
 tested = []
 
