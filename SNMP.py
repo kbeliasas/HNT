26aa5874297d968f085157b054ebbe43312a3fd7
kbeliasas <karolis.beliasas@gmail.com>
2018-01-14 23:10:32 +0200

    Draws topology

--- SNMP.py
+++ SNMP.py
@@ -38,7 +38,45 @@ def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
         failed.append(ip)
         return ans
 
+def draw_topology(graph, labels=None, graph_layout='spectral',
+               node_size=1600, node_color='blue', node_alpha=0.3,
+               node_text_size=12,
+               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
+               edge_text_pos=0.3,
+               text_font='sans-serif'):
+    G=nx.Graph()
+
+    for edge in graph:
+        G.add_edge(edge[0], edge[1])
+
+    if graph_layout == 'spring':
+        graph_pos=nx.spring_layout(G)
+    elif graph_layout == 'spectral':
+        graph_pos=nx.spectral_layout(G)
+    elif graph_layout == 'random':
+        graph_pos=nx.random_layout(G)
+    else:
+        graph_pos=nx.shell_layout(G)
+
+
+    nx.draw_networkx_nodes(G, graph_pos, node_size=node_size, alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G, graph_pos, width=edge_tickness, alpha=edge_alpha, edge_color=edge_color)
+    nx.draw_networkx_labels(G, graph_pos, font_size=node_text_size, font_family=text_font)
+
+    if labels is None:
+        labels = range(len(graph))
+
+    edge_labels = dict(zip(graph, labels))
+    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, label_pos=edge_text_pos)
+
+    plt.show()
+
+def lab_make(vari):
+    return vari
+
+
 tested = []
+tested_names = []
 
 
 ip_add = get_ip_add(ip)
@@ -47,6 +85,7 @@ for x in range(0, len(ip_add)):
     device_list.append(ip_add[x])
 all_list = [device_list]
 tested.append(ip)
+tested_names
 
 
 
@@ -97,9 +136,15 @@ while (x > -1):
                             z = -2
                             break
 
+for x in range(0, len(tested)):
+    for y in range(0, len(all_list[x])):
+        temp = (tested[x], all_list[x][y])
+        realations.append(temp)
 
+draw_topology(realations)
 
 print tested
 print all_list
+print failed
 
 print "end"
