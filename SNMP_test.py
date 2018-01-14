33fafb62c0ec7408dc628a7e5e9a6a19da1bd0e9
kbeliasas <karolis.beliasas@gmail.com>
2018-01-14 16:37:08 +0200

    Up

--- SNMP_test.py
+++ SNMP_test.py
@@ -2,7 +2,6 @@
 
 import easysnmp
 
-# ip = '192.168.226.152' # Pirmas irenginys
 ip = '192.168.50.100' # Testuojamas irenginys
 com = 'public'
 
@@ -12,8 +11,22 @@ def get_ip_add(ip):
     session = easysnmp.Session(hostname=ip, version=2, community=com)
     res = session.walk('.1.0.8802.1.1.2.1.4.2.1.3')
     ans = []
+    temp1 = []
     for item in res:
-        ans.append(item.oid)
+        temp1.append(item.oid)
+    c = 0
+    for item in temp1:
+        a = 0
+        b = 0
+        while a < 4:
+            if (item[len(item)-b] == '.'):
+                ans[c].append(item[len(item)-b])
+                a = a + 1
+                b = b + 1
+            else:
+                ans[c].append(item[len(item)-b])
+                b = b + 1
+        c = c + 1
     return ans
 
 ip_add = get_ip_add(ip)
