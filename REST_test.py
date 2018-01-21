9f67ae6041976b8c806a65566c4dedf495d7e2c9
kbeliasas <karolis.beliasas@gmail.com>
2018-01-21 16:03:03 +0200

    REST update

--- REST_test.py
+++ REST_test.py
@@ -7,7 +7,6 @@ h = httplib2.Http(".cache")
 h.add_credentials('admin', 'admin')
 resp, content = h.request('http://192.168.50.254:8181/restconf/operational/opendaylight-inventory:nodes', "GET")
 
-print "resp"
-print resp
-print "content"
-print content
+allnodes = json.loads(content)
+
+print allnodes
