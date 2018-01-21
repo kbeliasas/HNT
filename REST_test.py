2a47a546dc7fb1a4901c1eb7bf8c9e8e329fbd66
kbeliasas <karolis.beliasas@gmail.com>
2018-01-21 17:39:48 +0200

    Rest Update

--- REST_test.py
+++ REST_test.py
@@ -7,8 +7,21 @@ h = httplib2.Http(".cache")
 h.add_credentials('admin', 'admin')
 resp, content = h.request('http://192.168.50.254:8181/restconf/operational/opendaylight-inventory:nodes', "GET")
 
-allnodes = json.loads(content)
-nodes = allnodes['nodeProperties']
+all_OF_node_names = []
+all_OF_node_macs = []
+
+allOFnodes = json.loads(content)
+
+for x in range(0, len(allOFnodes['nodes']['node'])):
+    OF_node_names = []
+    OF_node_macs = []
+    for y in range(0, len(allOFnodes['nodes']['node'][x])):
+        OF_node_names.append(allOFnodes['nodes']['node'][x]['node-connector'][y]['id'])
+        OF_node_macs.append(allOFnodes['nodes']['node'][x]['node-connector'][y]['flow-node-inventory:hardware-address'])
+    all_OF_node_names.append(OF_node_names)
+    all_OF_node_macs.append(OF_node_macs)
 
 
-print nodes
+print all_OF_node_names
+print
+print all_OF_node_macs
