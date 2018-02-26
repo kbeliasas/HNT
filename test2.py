7bf045f12b1f94c960cb40e20a1e72620a77f2cf
kbeliasas <karolis.beliasas@gmail.com>
2018-02-26 12:13:30 +0200

    UP

--- test2.py
+++ test2.py
@@ -9,10 +9,7 @@ def get_man_ip_add(ip): #Management IP
     session = easysnmp.Session(hostname=ip, version=2, community=com)
     try:
         res = session.walk('.1.3.6.1.2.1.4.20.1.2')
-        ans = []
-        for item in res:
-            ans.append(item.value)
-        return ans
+        return res
     except Exception as e:
         ans = []
         ans.append('')
