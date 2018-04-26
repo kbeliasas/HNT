6af92b3f78a78dfc7d2b8fa890197e8dc33a2a50
kbeliasas <karolis.beliasas@gmail.com>
2018-04-26 13:19:12 +0300

    UP

--- test2.py
+++ test2.py
@@ -4,10 +4,13 @@ import easysnmp
 
 ip = '192.168.50.100'
 com = 'public'
+man_vlan = 500
 
 def get_man_ip_add(ip): #Management IP
     session = easysnmp.Session(hostname=ip, version=2, community=com)
     res = session.walk('.1.3.6.1.2.1.4.20.1.2')
+    for item in res:
+        print item.value
     return res
 
 print get_man_ip_add(ip)
