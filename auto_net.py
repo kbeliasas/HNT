989bb1891650b8981f77966f8c702739a94415df
kbeliasas <karolis.beliasas@gmail.com>
2018-04-26 13:37:37 +0300

    Up

--- auto_net.py
+++ auto_net.py
@@ -8,6 +8,7 @@ import json
 
 ip = '192.168.50.100'
 com = 'public'
+man_vlan = '500'
 
 def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
     session = easysnmp.Session(hostname=ip, version=2, community=com)
@@ -40,10 +41,13 @@ def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
         ans.append('failed')
         return ans
 
-def get_man_ip_add(ip):
+def get_man_ip_add(ip): #Management IP
     session = easysnmp.Session(hostname=ip, version=2, community=com)
     res = session.walk('.1.3.6.1.2.1.4.20.1.2')
-    return res
+    for item in res:
+        if item.value == man_vlan:
+            return item.oid_index
+            break
 
 
 tested = []
