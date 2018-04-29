e46e435bf468a38d1bea0e822958fe2fdaf6c340
kbeliasas <karolis.beliasas@gmail.com>
2018-04-29 19:15:57 +0300

    Up

---
+++ test3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import easysnmp
+
+ip = '192.168.50.3'
+com = 'public'
+man_vlan = '500'
+
+
+def get_man_ip_add(ip): #Management IP
+    try:
+        session = easysnmp.Session(hostname=ip, version=2, community=com)
+        res = session.walk('.1.3.6.1.2.1.4.20.1.2')
+        for item in res:
+            if item.value == man_vlan:
+                return item.oid_index
+                break
+    except Exception as e:
+        print 'Something wrong on ip = ' + ip
+        print(e)
+        ans = 'failed'
+        return ans
+
+
+
+
+print get_man_ip_add(ip)
