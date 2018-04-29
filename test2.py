bc7aea4350bcfaf6b0eee7a30b8df40b2e0bae81
kbeliasas <karolis.beliasas@gmail.com>
2018-04-29 14:24:57 +0300

    Up

--- test2.py
+++ test2.py
@@ -6,14 +6,6 @@ ip = '192.168.50.100'
 com = 'public'
 man_vlan = '500'
 
-def get_man_ip_add(ip): #Management IP
-    session = easysnmp.Session(hostname=ip, version=2, community=com)
-    res = session.walk('.1.3.6.1.2.1.4.20.1.2')
-    for item in res:
-        if item.value == man_vlan:
-            return item.oid_index
-            break
-
 def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
     session = easysnmp.Session(hostname=ip, version=2, community=com)
     try:
@@ -38,6 +30,21 @@ def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
             ans[c] = ans[c].lstrip('.')
             c = c + 1
         return ans
+    except Exception as e:
+        print 'Something wrong on ip = ' + ip
+        print(e)
+        ans = []
+        ans.append('failed')
+        return ans
+
+def get_man_ip_add(ip): #Management IP
+    try:
+        session = easysnmp.Session(hostname=ip, version=2, community=com)
+        res = session.walk('.1.3.6.1.2.1.4.20.1.2')
+        for item in res:
+            if item.value == man_vlan:
+                return item.oid_index
+                break
     except Exception as e:
         print 'Something wrong on ip = ' + ip
         print(e)
