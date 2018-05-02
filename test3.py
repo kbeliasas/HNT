b379b78d4552b876e5ea9c15105a8dbf53da2f8e
kbeliasas <karolis.beliasas@gmail.com>
2018-05-02 13:47:16 +0300

    Up

--- test3.py
+++ test3.py
@@ -11,9 +11,10 @@ def get_man_ip_add(ip): #Management IP
     try:
         session = easysnmp.Session(hostname=ip, version=2, community=com)
         res = session.walk('.1.3.6.1.2.1.4.20.1.2')
+        return res
         for item in res:
             if item.value == man_vlan:
-                return item.oid_index
+                #return item.oid_index
                 break
     except Exception as e:
         print 'Something wrong on ip = ' + ip
