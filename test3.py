f24aa618621050dc09fe5ca08bf862d2d549e457
kbeliasas <karolis.beliasas@gmail.com>
2018-05-18 18:58:11 +0300

    UP

--- test3.py
+++ test3.py
@@ -1,35 +1,8 @@
 #!/usr/bin/env python
 
-import easysnmp
+from ncclient import manager
 
-ip = '192.168.50.4'
-com = 'public'
-man_vlan = '500'
-
-
-def get_man_ip_add(ip): #Management IP
-    try:
-        session = easysnmp.Session(hostname=ip, version=2, community=com)
-        res = session.walk('.1.3.6.1.2.1.4.20.1.2')
-        res1 = None
-        for item in res:
-            if item.value == man_vlan:
-                res1 = item.oid_index
-                break
-        if res1 == None:
-            for item in res:
-                if item.value == str(int(float(man_vlan)) + 329):
-                    res1 = item.oid_index
-                    break
-        return res1
-    except Exception as e:
-        print 'Something wrong on ip = ' + ip
-        print(e)
-        ans = 'failed'
-        return ans
-
-
-
-if get_man_ip_add(ip) == None:
-    print "HIT"
-print get_man_ip_add(ip)
+with manager.connect(host=host, port=830, username=user, hostkey_verify=False) as m:
+    c = m.get_config(source='running').data_xml
+    with open("%s.xml" % host, 'w') as f:
+        f.write(c)
