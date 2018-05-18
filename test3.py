84a13e48a8e7c61075489807e86ae640e0fa26df
kbeliasas <karolis.beliasas@gmail.com>
2018-05-18 19:02:00 +0300

    UP

--- test3.py
+++ test3.py
@@ -2,6 +2,8 @@
 
 from ncclient import manager
 
+host = 192.168.50.5
+
 with manager.connect(host=host, port=830, username=user, hostkey_verify=False) as m:
     c = m.get_config(source='running').data_xml
     with open("%s.xml" % host, 'w') as f:
