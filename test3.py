bc1a06fc0a57a71e275d280c4790155be22e68f0
kbeliasas <karolis.beliasas@gmail.com>
2018-05-18 22:46:05 +0300

    UP

--- test3.py
+++ test3.py
@@ -2,14 +2,14 @@
 
 from ncclient import manager
 
-host = "192.168.50.5"
+host = "192.168.50.6"
 user = "admin"
 password = "cisco"
 
 
 with manager.connect(host=host, port=22, username=user, password=password, hostkey_verify=False, allow_agent=False, look_for_keys=False) as m:
     try:
-        print pavyko
+        print "pavyko"
         a = m.get()
         a.close()
     except Exception as e:
