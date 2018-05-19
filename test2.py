12a8bba66820554c057b999dd673fc591406baa2
kbeliasas <karolis.beliasas@gmail.com>
2018-05-19 20:03:38 +0300

    UP

--- test2.py
+++ test2.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
 import easysnmp
+#import networkx as nx
+#import matplotlib.pyplot as plt
+import httplib2
+import json
 
 ip = '192.168.50.100'
 com = 'public'
 man_vlan = '500'
+OF_swi = []
 
 def get_ip_add(ip): # Pasiima kaimyu adresus is irenginio.
     session = easysnmp.Session(hostname=ip, version=2, community=com)
@@ -57,27 +62,56 @@ def get_man_ip_add(ip): #Management IP
         ans = 'failed'
         return ans
 
+def get_port_macs(ip):
+    session = easysnmp.Session(hostname=ip, version=2, community=com, use_sprint_value=True)
+    res = session.walk('.1.3.6.1.2.1.2.2.1.6')
+    ans = []
+    for item in res:
+        ans.append(item.value)
+    return ans
+
+def mac_corr(string):
+    a = 0
+    x = 0
+    while (x > -1):
+        if (x < len(string)):
+            if (string[x] != ':'):
+                a = a + 1
+            elif(string[x] == ':'):
+                if (a < 2):
+                    a = 0
+                    string = string[:x-1] + '0' + string[x-1:]
+                    x = x + 1
+                elif (a >= 2):
+                    a = 0
+            x = x + 1
+        else:
+            if (a < 2):
+                string = string[:x-1] + '0' + string[x-1:]
+            x = -2
+            break
+    return string
+
+
 tested = []
 tested.append(ip)
 manage = []
+manage.append(get_man_ip_add(ip))
 if ip != get_man_ip_add(ip):
     ip = get_man_ip_add(ip)
     tested.append(ip)
-    manage.append(get_man_ip_add(ip))
 all_list = []
 temp_list = []
 for item in get_ip_add(ip):
     if get_man_ip_add(item) == 'failed':
-        print "1"
+        temp_list.append(item)
+        tested.append(item)
     elif get_man_ip_add(item) == None:
-        print "2"
-        print str(item) + "Don't have man VLAN"
+        print str(item) + " Don't have man VLAN"
+        temp_list.append(item)
+        tested.append(item)
     else:
         temp_list.append(get_man_ip_add(item))
 all_list.append(temp_list)
 
-
-
-print 'Manage = ' + str(manage)
-print 'all_list = ' + str(all_list)
-print 'tested = ' + str(tested)
+print "get_ip_add = %s" % get_ip_add(ip)
