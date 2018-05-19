8f364d96518b926b72f9f5e442f4fe50638a69b9
kbeliasas <karolis.beliasas@gmail.com>
2018-05-19 12:40:29 +0300

    UP

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import sys, os, warnings
-warnings.simplefilter("ignore", DeprecationWarning)
+import sys
+import logging
 from ncclient import manager
 
 host = "192.168.50.6"
@@ -9,7 +9,32 @@ user = "admin"
 password = "cisco"
 
 
-with manager.connect(host=host, port=22, username=user, password=password, hostkey_verify=False, allow_agent=False, look_for_keys=False) as m:
-    c = m.get_config(source='running').data_xml
-    with open("%s.xml" % host, 'w') as f:
-        f.write(c)
+ACCESS_VLAN = """
+<config>
+        <cli-config-data>
+            <cmd>interface %s</cmd>
+            <cmd>switchport mode access</cmd>
+            <cmd>switchport access vlan %s</cmd>
+        </cli-config-data>
+</config>
+"""
+
+def _check_response(rpc_obj, snippet_name):
+    log.debug("RPCReply for %s is %s" % (snippet_name, rpc_obj.xml))
+    xml_str = rpc_obj.xml
+    if "<ok />" in xml_str:
+        log.info("%s successful" % snippet_name)
+    else:
+        log.error("Cannot successfully execute: %s" % snippet_name)
+
+def create_access_vlan(host, user, password, interface, vlan):
+    with manager.connect(host=host, port=22, username=user, password=password, hostkey_verify=False, allow_agent=False, look_for_keys=False) as m:
+        try:
+            confstr = ACCESS_VLAN % (interface, vlan)
+            rpc_obj = m.edit_config(target='running', config=confstr)
+            _check_response(rpc_obj, 'ACCESS_VLAN')
+        except Exception:
+            log.exception("Exception in creating access port in %s for %s vlan" % (interface, vlan))
+
+create_access_vlan(host,user,password, "interface GigabitEthernet0/28", '500')
+
