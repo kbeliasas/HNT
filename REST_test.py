2a1121deac0c8653a8c41d0d8d075a97058cfc64
kbeliasas <karolis.beliasas@gmail.com>
2018-05-21 00:52:32 +0300

    UP

--- REST_test.py
+++ REST_test.py
@@ -90,6 +90,15 @@ h.add_credentials('admin', 'admin')
 resp, content = h.request('http://192.168.50.254:8181/restconf/operational/opendaylight-inventory:nodes/node/openflow:14193329701457472', "GET")
 
 
+all_OF_node_ports = []
+all_OF_node_macs = []
+
 allOFnodes = json.loads(content)
 
-print allOFnodes
+for x in range(0, len(allOFnodes['nodes']['node'])):
+    OF_node_macs = []
+    for y in range(0, len(allOFnodes['nodes']['node'][x])-2):
+        OF_node_macs.append(allOFnodes['nodes']['node'][x]['node-connector'][y]['flow-node-inventory:hardware-address'])
+    all_OF_node_macs.append(OF_node_macs)
+
+
